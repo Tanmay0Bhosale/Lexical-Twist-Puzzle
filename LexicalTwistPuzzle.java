64746a8d09f76ffeb51b4f422c65b83ab299dd7c
Tanmay <tanmaytanvi1234@gmail.com>
2026-02-20 11:48:32 +0530

    [Tanmay] : Added UC7 – Rule-Based Output (Unique Characters)

--- LexicalTwistPuzzle.java
+++ LexicalTwistPuzzle.java
@@ -1,4 +1,6 @@
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class LexicalTwistPuzzle {
 
@@ -16,14 +18,42 @@ public class LexicalTwistPuzzle {
 
         for (char ch : combined.toCharArray()) {
 
-            if ("AEIOU".indexOf(ch) != -1) {
-                vowels++;
-            } else if (Character.isLetter(ch)) {
-                consonants++;
-            }
+            if ("AEIOU".indexOf(ch) != -1) vowels++;
+            else if (Character.isLetter(ch)) consonants++;
         }
 
-        System.out.println("Vowels: " + vowels);
-        System.out.println("Consonants: " + consonants);
+        Set<Character> unique = new LinkedHashSet<>();
+        for (char ch : combined.toCharArray()) {
+            if (Character.isLetter(ch))
+                unique.add(ch);
+        }
+
+        if (vowels > consonants) {
+            printFirstTwo(unique, true);
+        } else if (consonants > vowels) {
+            printFirstTwo(unique, false);
+        } else {
+            System.out.println("Vowels and consonants are equal");
+        }
+    }
+
+    private static void printFirstTwo(Set<Character> set, boolean vowelMode) {
+
+        int count = 0;
+
+        for (char ch : set) {
+
+            if (vowelMode && "AEIOU".indexOf(ch) != -1) {
+                System.out.print(ch);
+                count++;
+            }
+
+            if (!vowelMode && "AEIOU".indexOf(ch) == -1) {
+                System.out.print(ch);
+                count++;
+            }
+
+            if (count == 2) break;
+        }
     }
 }
